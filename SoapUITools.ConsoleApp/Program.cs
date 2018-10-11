5e86d9474e3bc9d6108b614a866f1320a305a05c
Amael BERTEAU <amael.berteau@tech-easy.fr>
2018-10-11 18:43:29 +0200

    Test ProjectFileHelper

--- SoapUITools.ConsoleApp/Program.cs
+++ SoapUITools.ConsoleApp/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SoapUITools.Xml;
 using SoapUITools.Xml.Data;
 using SoapUITools.Xml.Definitions;
 using SoapUITools.Xml.Serialization;
@@ -11,12 +12,14 @@ namespace SoapUITools.ConsoleApp
 {
     class Program
     {
-        private const string Path = @"C:\Users\amael.berteau\OneDrive\Projets\SoapUITools\Technique\Examples\EasyNanny-WebAPI-soapui-project.xml";
+        private const string Path = @"E:\UserData\amael\OneDrive\Projets\SoapUITools\Technique\Examples\EasyNanny-WebAPI-soapui-project.xml";
+        private const string OutputPath = @"E:\UserData\amael\OneDrive\Projets\SoapUITools\Technique\Tests\EasyNanny-WebAPI-soapui-project.xml";
 
         static void Main(string[] args)
         {
             //Xml.Definitions.Project project = XmlSerializer.Deserialize(Path);
-            Xml.Data.Project project = XmlHelper.GetProject(Path);
+            //Xml.Data.Project project = XmlHelper.GetProject(Path);
+            ProjectFileHelper.OrderResourcesByName(Path, OutputPath);
             Console.ReadLine();
         }
     }
