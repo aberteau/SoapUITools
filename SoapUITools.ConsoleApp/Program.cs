673735600599453bbb6743f328191a545f355642
Amael BERTEAU <amael.berteau@tech-easy.fr>
2018-10-11 16:33:34 +0200

    Adding const Path in Program

--- SoapUITools.ConsoleApp/Program.cs
+++ SoapUITools.ConsoleApp/Program.cs
@@ -8,6 +8,8 @@ namespace SoapUITools.ConsoleApp
 {
     class Program
     {
+        private const string Path = @"C:\Users\amael.berteau\OneDrive\Projets\SoapUITools\Technique\Examples\EasyNanny-WebAPI-soapui-project.xml";
+
         static void Main(string[] args)
         {
 
